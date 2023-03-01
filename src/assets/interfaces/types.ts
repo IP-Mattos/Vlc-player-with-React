0839832390a60521dc80f6fe352e41d8ab906939
Ivan Ignacio Peña Mattos <76266733+IP-Mattos@users.noreply.github.com>
2023-03-01 16:51:53 -0300

    feat: create interfaces/ types for api

---
+++ src/assets/interfaces/types.ts
@@ -0,0 +1,30 @@
+export interface Track {
+    id: number
+    title: string
+    preview: string
+    artist: {
+        id: number
+        name: string
+        picture_big: string
+    }
+}
+
+export interface SearchResponse {
+    data: {
+        id: number
+        title: string
+        duration: number
+        link: string
+        preview: string
+        artist: {
+            id: number
+            name: string
+            picture_big: string
+        }
+        album: {
+            id: number
+            title: string
+            cover: string
+        }
+    }[]
+}
